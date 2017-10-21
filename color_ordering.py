8ce00cd1b13c35675e6490a83db7ed554fefae4f
Vinay Mehta <vinaymehta.nyc@gmail.com>
2017-10-20 20:37:53 -0400

    Added collage making. Also added source images

--- color_ordering.py
+++ color_ordering.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import math
 
 _PRIOR = 1
+mypath = ''
 
 def debug(prior, str):
     if(prior <= _PRIOR):
@@ -17,27 +18,19 @@ def magnitude(arr):
     squared_sum = math.sqrt(arr[0]**2 + arr[1]**2 + arr[2]**2)
     return squared_sum
 
-def main():
-    if (len(sys.argv) != 2):
-        print("Usage: python3 art.py <dir>")
-        return
-
-    # Get all images in a folder
-    mypath = sys.argv[1]
-    os.chdir(mypath)
+def dom_color():
     cwd = os.getcwd()
-    debug(3, cwd)
     onlyfiles = [f for f in os.listdir(cwd) if isfile(join(cwd, f))]
     onlyfiles = [x for x in onlyfiles if '.jpg' in x]
-    debug(2, onlyfiles)
+    onlyfiles.sort()
 
     # Run k-means on all images to find dominant colors
     dominant_colors = []
     for jpg in onlyfiles:
         debug(1, "Assessing {0}".format(jpg))
         image = cv2.imread(jpg)
-        debug(2, image.shape)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2Lab)
+        image = cv2.resize(image, (300,300))
 
         image = image.reshape((image.shape[0] * image.shape[1], 3))
         clt = KMeans(n_clusters = 1)
@@ -53,10 +46,63 @@ def main():
     data            = np.hstack((dominant_colors, magnitudes))
     data            = np.hstack((data, np.array(onlyfiles).reshape(len(onlyfiles),1)))
     data            = data[data[:,-2].argsort()]
-    debug(2, data)
 
-    for i in range(len(onlyfiles)):
+    return data
+
+def rename(data):
+    for i in range(data.shape[0]):
         os.rename(data[i,-1], "{:02d}.jpg".format(i))
+
+def collage():
+    cwd = os.getcwd()
+    onlyfiles = [f for f in os.listdir(cwd) if isfile(join(cwd, f))]
+    onlyfiles = [x for x in onlyfiles if '.jpg' in x]
+    onlyfiles.sort()
+
+    images = []
+    for jpg in onlyfiles:
+        img = cv2.imread(jpg)
+        img = cv2.resize(img, (700, 700))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        images.append(img)
+
+    final = []
+    x = 6
+    y = 4
+    for i in range(0,y):
+        for j in range(0,x):
+            if j > 0:
+                row = np.hstack((row, images[x*i+j]))
+            else:
+                row = images[x*i+j]
+
+        if i > 0:
+            final = np.vstack((final, row))
+        else:
+            final = row
+
+    plot = plt.figure()
+    plt.axis("off")
+    plt.imshow(final)
+    final = cv2.cvtColor(final, cv2.COLOR_RGB2BGR)
+    cv2.imwrite("collage.png",final)
+    plt.show()
+
+    cv2.imshow('final', final)
+
+def main():
+    if (len(sys.argv) != 2):
+        print("Usage: python3 art.py <dir>")
+        return
+
+    # Get all images in a folder
+    mypath = sys.argv[1]
+    os.chdir(mypath)
+
+    data = dom_color()
+    rename(data)
+    collage()
+    # reference_collage() TODO
 
 
 if __name__ == '__main__':
