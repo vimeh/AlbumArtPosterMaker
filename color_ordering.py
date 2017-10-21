e3fe437ecf0e94794463681b8049999f2f3f1e4b
Vinay Mehta <vinaymehta.nyc@gmail.com>
2017-10-20 23:26:37 -0400

    Reduced memory usage. Removed need to rename data

--- color_ordering.py
+++ color_ordering.py
@@ -7,8 +7,10 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import math
 
-_PRIOR = 1
-mypath = ''
+_PRIOR  = 1
+cwd     = ''
+mypath  = ''
+data    = []
 
 def debug(prior, str):
     if(prior <= _PRIOR):
@@ -28,15 +30,15 @@ def dom_color():
     dominant_colors = []
     for jpg in onlyfiles:
         debug(1, "Assessing {0}".format(jpg))
-        image = cv2.imread(jpg)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2Lab)
-        image = cv2.resize(image, (300,300))
+        image       = cv2.imread(jpg)
+        image       = cv2.cvtColor(image, cv2.COLOR_BGR2Lab)
+        image       = cv2.resize(image, (50,50))
 
-        image = image.reshape((image.shape[0] * image.shape[1], 3))
-        clt = KMeans(n_clusters = 1)
+        image       = image.reshape((image.shape[0] * image.shape[1], 3))
+        clt         = KMeans(n_clusters = 1)
         clt.fit(image)
 
-        color = clt.cluster_centers_[0]
+        color       = clt.cluster_centers_[0]
         dominant_colors.append(color)
 
     dominant_colors = np.array(dominant_colors)
@@ -49,16 +51,36 @@ def dom_color():
 
     return data
 
-def rename(data):
-    for i in range(data.shape[0]):
-        os.rename(data[i,-1], "{:02d}.jpg".format(i))
+def collage(data, x, y):
+    assert (x*y == data.shape[0])
 
-def collage():
-    cwd = os.getcwd()
-    onlyfiles = [f for f in os.listdir(cwd) if isfile(join(cwd, f))]
-    onlyfiles = [x for x in onlyfiles if '.jpg' in x]
-    onlyfiles.sort()
+    final = []
+    for i in range(0,y):
+        for j in range(0,x):
+            debug(1, 'Adding to collage ' + data[x*i+j,-1])
+            img         = cv2.imread(data[x*i+j,-1])
+            img         = cv2.resize(img, (700, 700))
+            img         = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            if j > 0:
+                row = np.hstack((row, img))
+            else:
+                row = img
 
+        if i > 0:
+            final = np.vstack((final, row))
+        else:
+            final = row
+
+    final = cv2.cvtColor(final, cv2.COLOR_RGB2BGR)
+    cv2.imwrite("collage.png", final)
+
+    cv2.namedWindow('final',cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('final', 900,900)
+    cv2.imshow('final', final)
+    cv2.waitKey(0);
+
+# Note: Order of images based on sorted list of image names
+def reference_collage():
     images = []
     for jpg in onlyfiles:
         img = cv2.imread(jpg)
@@ -66,7 +88,16 @@ def collage():
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         images.append(img)
 
-    final = []
+    final   = np.array([])
+    row     = np.array([])
+    # Row 1
+    row = cv2.i
+    # Row 2
+
+    # Row 3
+
+    # Row 4
+
     x = 6
     y = 4
     for i in range(0,y):
@@ -96,12 +127,12 @@ def main():
         return
 
     # Get all images in a folder
+    cwd = os.getcwd()
     mypath = sys.argv[1]
     os.chdir(mypath)
 
     data = dom_color()
-    rename(data)
-    collage()
+    collage(data, 6, 4)
     # reference_collage() TODO
 
 
